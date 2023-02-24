4621d85e56d52f741de248620f42253dfe6646a3
narutiga <100295602+narutiga@users.noreply.github.com>
2023-02-24 22:20:00 +0900

    refactor: move todoControl logic to separate file.

---
+++ src/controller/todosController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import prisma from "../lib/db";
+
+export const getTodos = async (req: Request, res: Response) => {
+  const todos = await prisma.todo.findMany();
+  return res.json(todos);
+};
+
+export const createTodo = async (req: Request, res: Response) => {
+  const { title } = req.body;
+  const todo = await prisma.todo.create({
+    data: {
+      id: "123",
+      title: "test",
+      user_id: "123",
+    },
+  });
+  res.json(todo);
+};
+
+export const getTodo = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  });
+  return res.json(todo);
+};
+
+export const updateTodo = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { title } = req.body;
+  const todo = await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      title,
+    },
+  });
+  return res.json(todo);
+};
+
+export const deleteTodo = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const todo = await prisma.todo.delete({
+    where: {
+      id,
+    },
+  });
+  return res.json({ message: "Todo deleted" });
+};
--- src/router/todos.ts
+++ src/router/todos.ts
@@ -1,58 +1,16 @@
-import express, { Request, Response } from "express";
-import prisma from "../lib/db";
+import express from "express";
+import {
+  createTodo,
+  deleteTodo,
+  getTodo,
+  getTodos,
+  updateTodo,
+} from "../controller/todosController";
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(async (req: Request, res: Response) => {
-    const todos = await prisma.todo.findMany();
-    return res.json(todos);
-  })
-  .post(async (req: Request, res: Response) => {
-    const { title } = req.body;
-    const todo = await prisma.todo.create({
-      data: {
-        id: "123",
-        title: "test",
-        user_id: "123",
-      },
-    });
-    res.json(todo);
-  });
+router.route("/").get(getTodos).post(createTodo);
 
-router
-  .route("/:id")
-  .get(async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const todo = await prisma.todo.findUnique({
-      where: {
-        id,
-      },
-    });
-    return res.json(todo);
-  })
-  .put(async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const { title } = req.body;
-    const todo = await prisma.todo.update({
-      where: {
-        id,
-      },
-      data: {
-        title,
-      },
-    });
-    return res.json(todo);
-  })
-  .delete(async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const todo = await prisma.todo.delete({
-      where: {
-        id,
-      },
-    });
-    return res.json({ message: "Todo deleted" });
-  });
+router.route("/:id").get(getTodo).put(updateTodo).delete(deleteTodo);
 
 module.exports = router;
