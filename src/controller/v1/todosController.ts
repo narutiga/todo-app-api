33621de42a12417e30636d4cd0dc3fd3eccd7865
narutiga <100295602+narutiga@users.noreply.github.com>
2023-02-26 20:14:12 +0900

    fix: add version prefix to endpoint. #7

--- src/controller/v1/todosController.ts
+++ src/controller/v1/todosController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import prisma from "../lib/db";
+import prisma from "../../lib/db";
 import { v4 as uuidv4 } from "uuid";
 export const getTodos = async (req: Request, res: Response) => {
   const todos = await prisma.todo.findMany();
