49c78f7653932414f6edf8e5cbd637ee0de7ef9f
narutiga <100295602+narutiga@users.noreply.github.com>
2023-03-04 14:38:27 +0900

    fix: fix return error message.

--- src/controller/v1/todosController.ts
+++ src/controller/v1/todosController.ts
@@ -8,14 +8,15 @@ export const getTodos = async (req: Request, res: Response) => {
 
 export const createTodo = async (req: Request, res: Response) => {
   const title = req.body.title;
-  const todo = await prisma.todo.create({
+  await prisma.todo.create({
     data: {
       id: uuidv4(),
       title,
       user_id: "123",
     },
   });
-  return res.json(todo);
+  const todos = await prisma.todo.findMany();
+  return res.json(todos);
 };
 
 export const getTodo = async (req: Request, res: Response) => {
@@ -31,13 +32,18 @@ export const getTodo = async (req: Request, res: Response) => {
 export const updateTodo = async (req: Request, res: Response) => {
   const id = req.params.id;
   const { title } = req.body;
-  const todo = await prisma.todo.update({
+  await prisma.todo.update({
     where: {
       id,
     },
     data: {
       title,
       updated_at: new Date(),
+    },
+  });
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
     },
   });
   return res.json(todo);
