e7a0ac4b076f23647adee4e734a8096b3c98bf40
narutiga <100295602+narutiga@users.noreply.github.com>
2023-03-21 13:17:44 +0900

    feat: add session management feature.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import logger from "./lib/winston/logger";
+import authRouter, { passport } from "./router/auth";
+import Redis from "ioredis";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import setUserData from "./middleware/setUserData";
 
-const app = express();
+const app: Application = express();
 
 const corsOptions = {
   origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   credentials: true,
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
 };
+
+const RedisStore = connectRedis(session);
+const url = (process.env.REDIS_URL as string) ?? "redis://localhost:6379";
+const client = new Redis(url);
+client.on("connect", () => {
+  console.log("Redis client connected");
+});
+client.on("error", (err) => {
+  console.log("Something went wrong " + err);
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -16,6 +31,19 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   logger.info(`${req.method} ${req.originalUrl}`);
   next();
 });
+app.use(
+  session({
+    store: new RedisStore({ client }),
+    secret: process.env.SESSION_SECRET ?? "secret",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/api/v1/auth", authRouter);
+app.use(setUserData);
 app.use("/api/v1/todos", require("./router/todos"));
 
 export default app;
