8e2164fc3b58ae818c8fd689a17727ac224d9e8c
narutiga <100295602+narutiga@users.noreply.github.com>
2023-04-02 18:00:28 +0900

    fix: update settings cookie.

--- src/app.ts
+++ src/app.ts
@@ -40,7 +40,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      domain: "todo-app-narutiga.vercel.app",
+      domain: "vercel.app",
       secure: true,
     },
   })
